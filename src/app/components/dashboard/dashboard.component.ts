16ae135f3a866b9d5d673f552de27d7b20e7eb47
Fulrr <65011212163@msu.ac.th>
2024-03-23 11:05:00 +0700

    Update signup

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavigationComponent } from '../navigation/navigation.component';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [  MatToolbarModule,
+              MatButtonModule,
+              MatInputModule,
+              MatFormFieldModule,
+              NavigationComponent,
+              RouterLink,
+              NgIf,
+              HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
