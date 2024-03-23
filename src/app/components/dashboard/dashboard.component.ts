e9918e27376a40311e0cd185c4052d5a5b9c1549
Fulrr <65011212163@msu.ac.th>
2024-03-23 11:56:39 +0700

    Update signup

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,9 +4,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavigationComponent } from '../navigation/navigation.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -23,8 +24,10 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
+  constructor(private authService: AuthService,
+    private router:Router,) {}
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
 
 }
