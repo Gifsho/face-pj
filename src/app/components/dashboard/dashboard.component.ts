03296d48958ed49d5c9ebde9492a3af78da18ab1
Fulrr <65011212163@msu.ac.th>
2024-03-23 14:08:02 +0700

    Update signup

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,7 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 
 
@@ -26,8 +27,33 @@ export class DashboardComponent implements OnInit {
   name: any;
   email: any;
 
+  constructor(private authService: AuthService,
+              private route: ActivatedRoute) {}
+
   
   ngOnInit(): void {
+    this.getUsedetail();
+  }
+
+  getUsedetail() {
+    this.route.queryParams.subscribe(params => {
+      // Get the value of 'email' parameter from the URL
+      this.userId = params['userId'];
+    });
+    this.authService.getUsedetail(this.userId)
+      .subscribe((response: any) => {
+
+        this.avatar_img = response?.avatar_img;
+        this.name = response?.name;
+        this.email = response?.email;
+        console.log(response?.avatar_img);
+        console.log(response?.name);
+        console.log(response?.email);
+
+      }, (error) => {
+        console.error("Error occurred while fetching user details:", error);
+      }
+      );
   }
 
 }
