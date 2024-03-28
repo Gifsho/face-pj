47feaa79a43ea1baf63ac14803dacd4b46f018bb
Fulrr <65011212163@msu.ac.th>
2024-03-28 12:26:48 +0700

    Update last

--- src/app/setting/main/main.component.ts
+++ src/app/setting/main/main.component.ts
@@ -89,7 +89,7 @@ export class MainComponent {
 
   changpw() {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.width = "550px";
+    dialogConfig.width = "1000px";
     this.dialog.open(ChangpasswordComponent,dialogConfig);
   }
 
