8b817602232b41a8889e708f28f626bed665025c
Fulrr <65011212163@msu.ac.th>
2024-03-28 18:11:36 +0700

    Update last

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -108,7 +108,6 @@ async getaccount(): Promise<any[]> {
 showimg(aid: any) {
   const dialogConfig = new MatDialogConfig();
   dialogConfig.width = "1000px";
-  dialogConfig.width = "1000px";
   dialogConfig.data = { aid: aid };
 
   this.dialog.open(ShowimgComponent,dialogConfig);
