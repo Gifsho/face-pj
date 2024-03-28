0f879a49b2dfdd31fd2c76a17038df9f94492fb3
Fulrr <65011212163@msu.ac.th>
2024-03-28 17:27:14 +0700

    Update last

--- src/app/components/dashboard/showimg/showimg.component.ts
+++ src/app/components/dashboard/showimg/showimg.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-showimg',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './showimg.component.html',
   styleUrl: './showimg.component.scss'
 })
-export class ShowimgComponent {
+export class ShowimgComponent implements OnInit {
+
+  imgAll: any = [];
+  aid:any; 
+
+
+  ngOnInit(): void {
+    this.aid = localStorage.getItem('aid');
+
+    fetch(`https://facemashbackend.onrender.com/img/fetchAllUserImg/${this.aid}`)
+      .then((response: Response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data: any) => {
+        // Store data in imgAll variable
+        this.imgAll = data;
+        console.log(this.imgAll); // Log the data stored in imgAll
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
 
 }
